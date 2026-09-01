9381d5b17467095192dbbcd00c5cf1eea1dc79b4
mayararosarodrigues-oss <mayara.rosa.rodrigues@escola.pr.gov.br>
2026-09-01 14:27:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ function mostraPergunta() {
   caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+(const alternativa of perguntaAtual.alternativas)
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
